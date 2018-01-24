14a8bb9bffb568b8e29bf47c520061476c26eebc
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2018-01-24 11:01:01 +0100

    not stable poke research

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,37 @@
 $(function () {
+    var isLoaded = false;
+    var pokemons = '';
     $.ajax({
         url: 'assets/pokemons.json',
         type: 'get',
         dataType: 'json',
+        async: false,
         success: function (data) {
-            for (var i in data) {
-                var students = document.querySelector('.asked-pokemon');
-                students.innerHTML += data[i].name + '<br>';
-            }
+            isLoaded = true;
+            pokemons = data;
+        },
+        error: function (data) {
+            var isLoaded = false;
         }
+
     });
+
+    if (isLoaded === true){
+        console.log("File loaded");
+        $('form').submit(function () {
+            var asked = $('input').val();
+            for (var i in pokemons){
+                if (asked === pokemons[i].name){
+                    console.log('Pokemon found : ', asked);
+                    console.log('Pokemon type : ', pokemons[i].type);
+                }else{
+                    console.log('Pokemon not found');
+                }
+            }
+        });
+    }else{
+        console.log('Fail to load file');
+    }
+
 
 });
